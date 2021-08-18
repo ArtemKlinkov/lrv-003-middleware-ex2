5e4eeff433ff0a95703a71324cc138ddd1e4e788
Artem <artemklinkov@yandex.ru>
2021-08-18 10:46:28 +0300

    003-middleware-ex2

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::group([
+        'middleware' => 'auth_info',
+    ], function () {
+        Route::get('/', function (\Illuminate\Http\Request  $request) {
+            return view('auth_info', ['user'=>$request->user]);
+        });
+    });
+});
